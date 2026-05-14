f15e3ee0614a149c4a697d90f9f53231bca31c2a
Lenny-09 <carolinekaruga@gmail.com>
2026-05-14 18:49:59 +0300

    conditional statement

---
+++ main.py
@@ -0,0 +1,5 @@
+number=-777
+if number > 0:
+    print('positive')
+else:
+    print('negative')
---
+++ profit.py
@@ -0,0 +1,8 @@
+buying_price=float(input('enter the buying price'))
+selling_price=float(input('enter the selling price'))
+
+
+if (selling_price > buying_price):
+    print('profit')
+else:
+    print('no_profit')
